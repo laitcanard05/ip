ca4e0b52c47cf539e62ae9fd214955e10a0fc7e6
laitcanard05 <winyx03@gmail.com>
2025-02-04 23:43:21 +0800

    Add code to store text and display text

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         String line;
+        String[] tasks = new String[100];
+        int taskIndex = 0;
         Scanner in = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,7 +16,15 @@ public class Bob {
         line = in.nextLine();
 
         while (!line.equalsIgnoreCase("bye")) {
-            System.out.println(line);
+            if (line.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskIndex; i++) {
+                    System.out.println((i+1) + ". " + tasks[i]);
+                }
+            } else{
+                tasks[taskIndex] = line;
+                taskIndex++;
+                System.out.println("added: " + line);
+            }
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
