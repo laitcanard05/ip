ab33ff124159eae933c15ec3d5a22ac292acadf1
laitcanard05 <winyx03@gmail.com>
2025-02-04 23:19:09 +0800

    Add output message to display when user starts chatbot, rename chatbot

---
+++ src/main/java/Bob.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(logo);
+        System.out.println("Hello! I'm Bob!\nWhat can I do for you?");
+        line = in.nextLine();
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+}
