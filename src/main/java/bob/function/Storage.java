5d24286ba3fa4f9674e7d45b506db81b820e8703
laitcanard05 <winyx03@gmail.com>
2025-03-04 14:23:22 +0800

    added Javadoc comments

--- src/main/java/bob/function/Storage.java
+++ src/main/java/bob/function/Storage.java
@@ -13,6 +13,14 @@ public class Storage {
     File savedTasks;
     String filepath;
 
+    /**
+     * Constructs a Storage instance with the specified file path.
+     * Initializes the file for storing tasks. If the file does not exist,
+     * it attempts to create the necessary directories and the file.
+     *
+     * @param filepath The file path of the file where tasks will be stored.
+     * @throws IOException If an I/O error occurs while creating the file or directories.
+     */
     public Storage(String filepath) throws IOException {
         this.filepath = filepath;
         savedTasks = new File(filepath);
@@ -22,6 +30,14 @@ public class Storage {
         }
     }
 
+    /**
+     * Returns a single string representation of the tasks loaded from the saved file,
+     * with each task on a new line.
+     * Each task is read line by line and appended to the resulting string.
+     *
+     * @return string representation of loaded tasks
+     * @throws FileNotFoundException If the saved tasks file does not exist.
+     */
     public String loadTasks() throws FileNotFoundException {
         Scanner reader = new Scanner(savedTasks);
         String savedTasksAsString = "";
@@ -31,6 +47,14 @@ public class Storage {
         return savedTasksAsString;
     }
 
+    /**
+     * Returns a formatted string representation of the task list,
+     * with each task on a new line, to allow the current task list to
+     * be saved in a .txt file.
+     *
+     * @return the formatted string representation of the task list
+     * @param taskList The TaskList to be converted into text format.
+     */
     public String getTaskListAsText(TaskList taskList) {
         String taskListAsText = "";
         int numTasks = taskList.getNumTasks();
@@ -46,6 +70,13 @@ public class Storage {
         return taskListAsText;
     }
 
+    /**
+     * Saves the given TaskList to a file by writing its text representation.
+     * The existing content of the file is overwritten.
+     *
+     * @param taskList The TaskList to be saved.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
     public void saveTasks(TaskList taskList) throws IOException {
         FileWriter fw = new FileWriter(filepath);
         fw.write(getTaskListAsText(taskList));
